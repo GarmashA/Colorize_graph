d38ea4f7257166aceb9fe18237b5a74518a652ab
GarmashA <104096970+GarmashA@users.noreply.github.com>
2022-05-10 16:16:51 +0300

    Финальная версия main
    Часть с выводом всех цветов закоменчена

--- main.cpp
+++ main.cpp
@@ -1,46 +1,41 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
+namespace fs = filesystem;
 
 vector<int> colorize (vector<vector<int> >& graph, vector<int>& edges_counter);
+vector<vector<int>> read_graph(const string& name_file, vector<pair<int, int>>& list_edges, vector<int>& edges_counter);
 
-int main() {
-    int vertex, edges;
-    vector<pair<int, int> > pairs;
-    vector<int> edges_counter;
-    pairs.clear();
-    edges_counter.clear();
-    cin >> vertex >> edges;
-    vector<vector<int> > graph;
-    vector<int> v(vertex, 0);
-    graph.resize(vertex, v);
-    auto zero = make_pair(0, 0);
-    pairs.resize(graph.size(), zero);
-    for (int i = 0; i < edges; i++) {
-        int start, end;
-        cin >> start >> end;
-        auto p1 = make_pair(start, end);
-        pairs.push_back(p1);
-        graph[start][end] = 1;
-        graph[end][start] = 1;
-        pairs[start].first += 1;
-        pairs[end].first += 1;
-        pairs[start].second = start;
-        pairs[end].second = end;
-    }
-    sort(pairs.begin(), pairs.end());
-    for (int i = pairs.size() - 1; i >= 0; i--) {
-        edges_counter.push_back(pairs[i].second);
-    }
-    vector<int> colored = colorize(graph, edges_counter);
-    int max = -1;
-    for (int i = 0; i < colored.size(); i++) {
-        if (colored[i] > max) {
-            max = colored[i];
+int main() {    
+    string path = "C:\Users\garmash\Desktop\colorize";
+    auto it = fs::directory_iterator(path);
+    vector<fs::path> array_path;
+    copy_if(fs::begin(it), fs::end(it), std::back_inserter(array_path),
+        [](const auto& entry) {
+            return fs::is_regular_file(entry);
+    });
+    for (auto & p : array_path) {
+        vector<int> edges_counter;
+        vector<pair<int, int>> edges;
+        vector<vector<int>> graph = read_graph(p.string(), edges, edges_counter);
+        vector<int> colored2 = colorize(graph, edges_counter);
+        int max = -1;
+        for (int i = 0; i < colored2.size(); i++) {
+            if (colored2[i] > max) {
+                max = colored2[i];
+            }
         }
+        cout << p.string() << endl;
+        cout << max + 1 << endl;
+        /*    для вывода цветов всех вершин
+        for (auto i : colored2) {
+            cout << i << " ";
+        }
+        cout << endl;
+         */
     }
-    cout << max + 1;
     return 0;
 }
